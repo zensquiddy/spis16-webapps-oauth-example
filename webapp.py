d3a99a5cab64fb2d2416bb4fe286e3bd9be09173
sky-adams <skyadams@sbunified.org>
2017-06-28 12:25:47 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -22,7 +22,7 @@ app = Flask(__name__)
 
 app.debug = True
 
-app.config.update(dict(PREFERRED_URL_SCHEME = 'https'))
+#app.config.update(dict(PREFERRED_URL_SCHEME = 'https'))
 
 app.secret_key = os.environ['APP_SECRET_KEY']
 oauth = OAuth(app)
@@ -64,7 +64,7 @@ def home():
 
 @app.route('/login')
 def login():
-    return github.authorize(callback=url_for('authorized', _external=True))
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/logout')
 def logout():
