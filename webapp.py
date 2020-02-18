2c7ad4de0eee00b69bda2fb0050e359f38d90abf
sky-adams <skyadams@sbunified.org>
2020-02-18 08:03:26 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -64,7 +64,7 @@ def authorized():
     else:
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
-            session['user_data']=github['user'].data
+            session['user_data']=github.get('user').data
             #pprint.pprint(vars(github['/email']))
             #pprint.pprint(vars(github['api/2/accounts/profile/']))
             message='You were successfully logged in as ' + session['user_data']['login'] + '.'
